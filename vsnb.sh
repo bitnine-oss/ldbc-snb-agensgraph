03d7aa6d6243f802af5675874109a1d69cd262cf
Kisung Kim <kskim@bitnine.net>
2017-01-02 07:32:50 +0900

    Refactor: modify script and properties for validation

--- vsnb.sh
+++ vsnb.sh
@@ -5,6 +5,5 @@ target/classes/jeeves-0.3-SNAPSHOT.jar\
 :target/classes/agensgraph-jdbc-0.9.0-SNAPSHOT.jar\
 :target/classes \
 com.ldbc.driver.Client \
--P ag-ldbc_driver-snb-validation_parameter_creation.properties \
+-P ldbc_snb_validation.properties \
 -P ag_jdbc.properties \
--P /home/ktlee/tools/ldbc_snb_datagen/social_network/updateStream.properties
